a2a305027ca2a74d7fa0483f82df8742895ca5dd
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-10-03 10:39:14 +0200

    Header showing logo search and chat icons

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import styled from 'styled-components/native'
+import 'react-native-gesture-handler'
+
+import Header from './components/Header.js';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar
+      backgroundColor='#FFFFFF'
+      barStyle='dark-content'
+      />
+      <ScrollView>
+        <Header/>
+      </ScrollView>
+    </>
   );
 }
 
