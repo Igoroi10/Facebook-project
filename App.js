b4ba147bb6bb4b9a194a0c1c12ab2a4decd5378a
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-10-04 08:57:38 +0200

    [NEW] scroll menu implementation [TODO] fix gap between header and menu

--- App.js
+++ App.js
@@ -30,33 +30,39 @@ export default function App() {
         <Header/>
       </ScrollView>
       <NavigationContainer>
-        <Tab.Navigator
-          swipeEnabled='true'
-          tabBarOptions=
-          screenOptions={{
+        <Tab.Navigator          
+          options={
+            {
+              tabBarActiveTintColor: "#3a86e9",
+              tabBarInactiveTintColor: "#9f9f9f",
+              tabBarShowLabel: false,
+              tabBarShowIcon: true,
+              swipeEnabled: true,
+            }
+          }
+          screenOptions={({route})=> ({
             tabBarIcon: ({focused, color, size}) => {
               let iconName;
               if(route.name === 'Home') iconName = 'home'
-              else if(route.name === 'Friends') iconName = 'account-multiple-cutline'
-              else if(route.name === 'Profile') iconName = 'account-group'
-              else if(route.name === 'Profile') iconName = 'account-group'
-              else if(route.name === 'Profile') iconName = 'account-group'
-              else if(route.name === 'Profile') iconName = 'account-group'
+              else if(route.name === 'Friends') iconName = 'account-multiple-outline'
+              else if(route.name === 'Groups') iconName = 'account-group'
+              else if(route.name === 'Profile') iconName = 'account-circle-outline'
+              else if(route.name === 'Notifications') iconName = 'bell-outline'
+              else if(route.name === 'Menu') iconName = 'menu'
               return <MaterialCommunityIcons name={iconName} size={26} color={color}/>
             },
-          }}>
-
-          </Tab.Navigator>
+          })}
+          >
+          <Tab.Screen name='Home' component={Home}/>
+          <Tab.Screen name='Friends' component={Friends}/>
+          <Tab.Screen name='Groups' component={Groups}/>
+          <Tab.Screen name='Profile' component={Profile}/>
+          <Tab.Screen name='Notifications' component={Notifications}/>
+          <Tab.Screen name='Menu' component={Menu}/>
+        </Tab.Navigator>
       </NavigationContainer>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
